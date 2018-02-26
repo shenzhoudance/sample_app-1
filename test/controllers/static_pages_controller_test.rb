373d00302b1478d0058e7b5d4ee46744dc95a23f
shanfeng094 <1842114650@qq.com>
2018-02-26 17:55:55 +0800

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,17 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title","Home | Bilibili"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title","Help | Bilibili"
   end
-
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title","About | Bilibili"
+  end
 end
